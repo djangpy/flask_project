94f91bde82a1902b19dac2605437dea9ad758f0f
Djangpy <djangupy@gmail.com>
2019-04-22 18:48:50 +0000

    add gitignore

--- mdblog/app.py
+++ mdblog/app.py
@@ -5,9 +5,18 @@ flask_app = Flask(__name__)
 
 
 @flask_app.route("/")  # Dekorator spoji funkciu index s adresou
-def index():
-	return render_template("welcome_page.jinja")
+def view_velcome_page():
+	return render_template("welcome_page.jinja", active='home')
+
+@flask_app.route("/about/")
+def view_about_page():
+	return render_template("about.jinja")
+
+@flask_app.route("/articles/")
+def view_articles_page():
+	return render_template("articles.jinja")
 
 @flask_app.route("/admin/")
-def view_admin():
-	return "Hello Administrator"
+def view_admin_page():
+	return render_template("admin.jinja")
+
